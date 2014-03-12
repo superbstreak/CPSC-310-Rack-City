28b2c3db225d5d77b1b2655b2fc679136c36cc3e
superbstreak <robwu15@gmail.com>
2014-03-11 18:49:56 -0700

    DataStore upload entity function (from tutorial, refine)

--- Rack City/src/cs310MRAK/rackcity/server/rackcityService.java
+++ Rack City/src/cs310MRAK/rackcity/server/rackcityService.java
@@ -6,23 +6,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
 
 public class rackcityService extends HttpServlet {
+
+public Key rackKEY;
 	
 	public void doGet (HttpServletRequest request, HttpServletResponse result) throws IOException
 	{
 		// tutorial
-		Entity e = new Entity("BikeRack");
-		e.setProperty("Address", "400 N 23RD AVE");
-		e.setProperty("LatLng", "49.27417,-123.13098");
-		e.setProperty("NumberofRack", 3);
-		// entiy with attrs
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		
+		// use this to create different data
+		
+		//================================ UPLOAD ===================================
+		
+		// loop this for entry
+		newRack("400 N 24th AVEujkm ", "49.27417,-123.13098", 5, ds); //create new entry
+		
+		//------- DRAFT ---------
+		// entiy with attrs, group these
+		//Entity e = new Entity("BikeRack");					// identifier (have uid)
+		//e.setProperty("Address", "400 N 23RD AVE");			// attribute values
+		//e.setProperty("LatLng", "49.27417,-123.13098");		// attribute values	
+		//e.setProperty("NumberofRack", 3);					// attribute values
+		
+		//upload entity
+		//ds.put(e);
+		
+		//=============================== DOWNLOAD ==================================
+		Key k = rackKEY;
+		try 													// try to get entity with this key from server
+		{
+			Entity e = ds.get(k);
+			
+		} 
+		catch (EntityNotFoundException e) 						// failed, error message! SHOULD NOT HAPPEN IN FINAL PRODUCT!
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//===========================================================================
 		
 		result.setContentType("text/plain");
 		result.getWriter().println("Test Print Values");
-		
-
+	}
+	
+	public void newRack (String address, String LL, int Racknum, DatastoreService db)
+	{
+		Entity e = new Entity("Rack");
+		rackKEY = e.getKey();
+		e.setProperty("Address", address);
+		e.setProperty("LatLng", LL);
+		e.setProperty("NoR", Racknum);
+		db.put(e);
 	}
 	
 }
