fe3300b50ea7e0040f773e50c433078db5343504
a2g7 <kevin.david.greer@gmail.com>
2014-03-13 17:32:21 -0700

    Remove duplicated directory

--- src/cs310MRAK/rackcity/client/StolenBikesServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package cs310MRAK.rackcity.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface StolenBikesServiceAsync {
-
-//	void addStolenBike(String rackID AsyncCallback callback);//, GeoPoint coordinate, AsyncCallback callback);
-
-}
