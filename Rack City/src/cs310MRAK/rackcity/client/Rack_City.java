1ddd450563625e2c746fb1c46be62c730656e0f6
superbstreak <robwu15@gmail.com>
2014-03-13 18:16:20 -0700

    new KEY

--- Rack City/src/cs310MRAK/rackcity/client/Rack_City.java
+++ Rack City/src/cs310MRAK/rackcity/client/Rack_City.java
@@ -245,7 +245,7 @@ public class Rack_City implements EntryPoint {
 
 	private void loadGoogleMap(){
 
-		//MAP API KEY: AIzaSyDaxfDkKyFywp7WKW1Bv07mrH9QNBup-04 (Might not work)
+		//MAP API KEY: AIzaSyCOopo9nSwdUPDKLay6JVPCzzD4ruAwmG0 (Might not work)
 		Maps.loadMapsApi("", "2", false, new Runnable() { public void run() {
 			// This code is used to create lat/long data points
 			LatLng def = LatLng.newInstance(49.249697, -123.139098); //set default
