55c49ea24f2ccc1071f2fe7ca5725270830a9f4e
TunaObject <ObedientWorker@gmail.com>
2014-03-21 13:44:14 -0700

    code for csv -> xml example

--- Rack City/src/cs310MRAK/rackcity/server/FTPserviceImpl.java
+++ Rack City/src/cs310MRAK/rackcity/server/FTPserviceImpl.java
@@ -57,6 +57,53 @@ public class FTPserviceImpl extends RemoteServiceServlet implements FTPservice{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+		        
+		        // ==================================    
+		        // *** CSV TO XML CONVERTER ***
+		        // ==================================
+		        
+//		        BufferedReader reader = new BufferedReader(new InputStreamReader(
+//		        		Csv2Xml.class.getResourceAsStream("test.csv")));
+//		        StringBuilder xml = new StringBuilder();
+//		        String lineBreak = System.getProperty("line.separator");
+//		        String line = null;
+//		        List<String> headers = new ArrayList<String>();
+//		        boolean isHeader = true;
+//		        int count = 0;
+//		        int entryCount = 1;
+//		        xml.append("<root>");
+//		        xml.append(lineBreak);
+//		        while ((line = reader.readLine()) != null) {
+//		        	StringTokenizer tokenizer = new StringTokenizer(line, ",");
+//		        	if (isHeader) {
+//		        		isHeader = false;
+//		        		while (tokenizer.hasMoreTokens()) {
+//		        			headers.add(tokenizer.nextToken());
+//		        		}
+//		        	} else {
+//		        		count = 0;
+//		        		xml.append("\t<entry id=\"");
+//		        		xml.append(entryCount);
+//		        		xml.append("\">");
+//		        		xml.append(lineBreak);
+//		        		while (tokenizer.hasMoreTokens()) {
+//		        			xml.append("\t\t<");
+//		        			xml.append(headers.get(count));
+//		        			xml.append(">");
+//		        			xml.append(tokenizer.nextToken());
+//		        			xml.append("</");
+//		        			xml.append(headers.get(count));
+//		        			xml.append(">");
+//		        			xml.append(lineBreak);
+//		        			count++;
+//		        		}
+//		        		xml.append("\t</entry>");
+//		        		xml.append(lineBreak);
+//		        		entryCount++;
+//		        	}
+//		        }
+//		        xml.append("</root>");
+//		        System.out.println(xml.toString());
 			      
 	}
 
