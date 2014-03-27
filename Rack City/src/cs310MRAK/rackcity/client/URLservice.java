72d68606e1c7730cb0b1d96a25bd4fb334c0786d
TunaObject <ObedientWorker@gmail.com>
2014-03-27 12:53:54 -0700

    task 72. successfully pulling xml document from dropbox public folder

---
+++ Rack City/src/cs310MRAK/rackcity/client/URLservice.java
@@ -0,0 +1,11 @@
+package cs310MRAK.rackcity.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("grabData")
+public interface URLservice extends RemoteService{
+
+public void adminConnection();
+
+}
