0a971f11d0d3ca23210bbed81d8a14b6e92f252b
aprad1124 <abhisek_pradhan@hotmail.com>
2014-04-02 19:14:47 -0700

    Added template for parser tests.

---
+++ Rack City/src/parsertests/ParserTests.java
@@ -0,0 +1,14 @@
+package parsertests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
